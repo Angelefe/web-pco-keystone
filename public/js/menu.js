882e499ffc53af139a217e99974b5e7b9754e8fe
Castor <daniellenarduzzi@gmail.com>
2018-05-31 23:41:34 -0300

    cambiado el selector, submenu funcionando

--- public/js/menu.js
+++ public/js/menu.js
@@ -2,8 +2,7 @@
 $ (document).ready(function(){
 if ($(window).width() < 1205){
     $ (".submenu").click(function(){
-    $(this).children("ul").slideToggle();
-
+    $("ul", this).slideToggle();
   })
   $ ("ul").click(function(p){
     p.stopPropagation();
